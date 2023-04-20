d8b8833528352e64b0367079b7f8eddc5091e74f
Aleksandr Dronov <dronovspb@gmail.com>
2023-04-20 18:02:50 +0300

    ver.2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,16 +1,25 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env.deploy') });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
 
 module.exports = {
   apps: [{
     name: 'frontend',
-    script: './build/index.hnml',
+    script: './build/index.html',
   }],
 
   // Настройка деплоя
   deploy: {
     production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
       repo: 'git@github.com:AleksandrDronov/web-plus-pm2-deploy.git',
-      'post-deploy': 'cd /home/dronov/web-plus-pm2-deploy/source/frontend && npm i && npm run build && npx pm2 restart ecosystem.config.js',
+      path: DEPLOY_PATH,
+      'post-deploy': `cd ${DEPLOY_PATH}source/frontend && npm i && npm run build && npx pm2 restart ecosystem.config.js`,
     },
   },
 };
