0c9703529ae93004824955ae426820aff5a4d3c5
Aleksandr Dronov <dronovspb@gmail.com>
2023-04-20 17:12:38 +0300

    ver.2

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       repo: 'git@github.com:AleksandrDronov/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend && scp .env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build`,
     },
   },
 };
