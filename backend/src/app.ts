a8360a3fcc51491c6d6ec8f873e9396e94c044a4
Aleksandr Dronov <dronovspb@gmail.com>
2023-04-21 12:54:27 +0300

    ver3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors());
+app.use(cors({ origin: 'https://mesto-project.nomoredomains.monster/' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
